c193d3a2dbbd3e385c0bfd7ba69e833156d75632
Thomas Piccirello <thomas@doppler.com>
2019-10-22 00:18:16 -0700

    Implement secrets set and secrets delete commands
    Consolidate printing logic

--- cmd/secrets.go
+++ cmd/secrets.go
@@ -25,6 +25,7 @@ import (
 	"io/ioutil"
 	"path"
 	"path/filepath"
+	"sort"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -44,52 +45,9 @@ var secretsCmd = &cobra.Command{
 		raw := utils.GetBoolFlag(cmd, "raw")
 
 		localConfig := configuration.LocalConfig(cmd)
+		_, secrets := api.GetAPISecrets(cmd, localConfig.Key.Value, localConfig.Project.Value, localConfig.Config.Value)
 
-		if jsonFlag {
-			response, _ := api.GetAPISecrets(cmd, localConfig.Key.Value, localConfig.Project.Value, localConfig.Config.Value, false)
-			fmt.Println(string(response))
-			return
-		}
-
-		_, secrets := api.GetAPISecrets(cmd, localConfig.Key.Value, localConfig.Project.Value, localConfig.Config.Value, true)
-
-		if plain {
-			sbEmpty := true
-			var sb strings.Builder
-			for _, secret := range secrets {
-				if sbEmpty {
-					sbEmpty = false
-				} else {
-					sb.WriteString("\n")
-				}
-
-				if raw {
-					sb.WriteString(secret.RawValue)
-				} else {
-					sb.WriteString(secret.ComputedValue)
-				}
-			}
-
-			fmt.Println(sb.String())
-			return
-		}
-
-		headers := []string{"name", "value"}
-		if raw {
-			headers = append(headers, "raw")
-		}
-
-		var rows [][]string
-		for _, secret := range secrets {
-			row := []string{secret.Name, secret.ComputedValue}
-			if raw {
-				row = append(row, secret.RawValue)
-			}
-
-			rows = append(rows, row)
-		}
-
-		utils.PrintTable(headers, rows)
+		printSecrets(secrets, []string{}, jsonFlag, plain, raw)
 	},
 }
 
@@ -110,72 +68,78 @@ doppler secrets get api_key crypto_key`,
 		raw := utils.GetBoolFlag(cmd, "raw")
 
 		localConfig := configuration.LocalConfig(cmd)
-		_, secrets := api.GetAPISecrets(cmd, localConfig.Key.Value, localConfig.Project.Value, localConfig.Config.Value, true)
+		_, secrets := api.GetAPISecrets(cmd, localConfig.Key.Value, localConfig.Project.Value, localConfig.Config.Value)
 
-		if jsonFlag {
-			secretsMap := make(map[string]map[string]string)
-			for _, name := range args {
-				if secrets[name] != (api.ComputedSecret{}) {
-					secretsMap[name] = make(map[string]string)
-					secretsMap[name]["raw"] = secrets[name].RawValue
-					secretsMap[name]["computed"] = secrets[name].ComputedValue
-				}
-			}
+		printSecrets(secrets, args, jsonFlag, plain, raw)
+	},
+}
 
-			resp, err := json.Marshal(secretsMap)
-			if err != nil {
-				utils.Err(err)
-			}
+var secretsSetCmd = &cobra.Command{
+	Use:   "set [secrets]",
+	Short: "Set the value of one or more secrets",
+	Long: `Set the value of one or more secrets.
 
-			fmt.Println(string(resp))
-			return
+Ex: set the secrets "api_key" and "crypto_key":
+doppler secrets set api_key=123 crypto_key=456`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			dopplerErrors.CommandMissingArgument(cmd)
 		}
 
-		var matchedSecrets []api.ComputedSecret
-		for _, name := range args {
-			if secrets[name] != (api.ComputedSecret{}) {
-				matchedSecrets = append(matchedSecrets, secrets[name])
+		jsonFlag := utils.GetBoolFlag(cmd, "json")
+		plain := utils.GetBoolFlag(cmd, "plain")
+		raw := utils.GetBoolFlag(cmd, "raw")
+		silent := utils.GetBoolFlag(cmd, "silent")
+
+		secrets := make(map[string]interface{})
+		var keys []string
+		for _, arg := range args {
+			secretArr := strings.Split(arg, "=")
+			keys = append(keys, secretArr[0])
+			if len(secretArr) < 2 {
+				secrets[secretArr[0]] = ""
+			} else {
+				secrets[secretArr[0]] = secretArr[1]
 			}
 		}
 
-		if plain {
-			sbEmpty := true
-			var sb strings.Builder
-			for _, secret := range matchedSecrets {
-				if sbEmpty {
-					sbEmpty = false
-				} else {
-					sb.WriteString("\n")
-				}
+		localConfig := configuration.LocalConfig(cmd)
+		_, response := api.SetAPISecrets(cmd, localConfig.Key.Value, localConfig.Project.Value, localConfig.Config.Value, secrets)
 
-				if raw {
-					sb.WriteString(secret.RawValue)
-				} else {
-					sb.WriteString(secret.ComputedValue)
-				}
-			}
+		if !silent {
+			printSecrets(response, keys, jsonFlag, plain, raw)
+		}
+	},
+}
 
-			fmt.Println(sb.String())
-			return
+var secretsDeleteCmd = &cobra.Command{
+	Use:   "delete [secrets]",
+	Short: "Delete the value of one or more secrets",
+	Long: `Delete the value of one or more secrets.
+
+Ex: delete the secrets "api_key" and "crypto_key":
+doppler secrets delete api_key crypto_key`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			dopplerErrors.CommandMissingArgument(cmd)
 		}
 
-		headers := []string{"name", "value"}
-		if raw {
-			headers = append(headers, "raw")
+		jsonFlag := utils.GetBoolFlag(cmd, "json")
+		plain := utils.GetBoolFlag(cmd, "plain")
+		raw := utils.GetBoolFlag(cmd, "raw")
+		silent := utils.GetBoolFlag(cmd, "silent")
+
+		secrets := make(map[string]interface{})
+		for _, arg := range args {
+			secrets[arg] = nil
 		}
 
-		var rows [][]string
-		for _, secret := range matchedSecrets {
-			// row := []string{secret.Name, "                  " + secret.ComputedValue}
-			row := []string{secret.Name, secret.ComputedValue}
-			if raw {
-				row = append(row, secret.RawValue)
-			}
+		localConfig := configuration.LocalConfig(cmd)
+		_, response := api.SetAPISecrets(cmd, localConfig.Key.Value, localConfig.Project.Value, localConfig.Config.Value, secrets)
 
-			rows = append(rows, row)
+		if !silent {
+			printSecrets(response, []string{}, jsonFlag, plain, raw)
 		}
-
-		utils.PrintTable(headers, rows)
 	},
 }
 
@@ -221,17 +185,114 @@ doppler secrets download /root/test.env`,
 }
 
 func init() {
+	secretsCmd.Flags().String("project", "", "doppler project (e.g. backend)")
+	secretsCmd.Flags().String("config", "", "doppler config (e.g. dev)")
 	secretsCmd.Flags().Bool("plain", false, "print values without formatting")
 	secretsCmd.Flags().Bool("raw", false, "print the raw secret value without processing variables")
 	secretsCmd.Flags().Bool("json", false, "output json")
 
+	secretsGetCmd.Flags().String("project", "", "doppler project (e.g. backend)")
+	secretsGetCmd.Flags().String("config", "", "doppler config (e.g. dev)")
 	secretsGetCmd.Flags().Bool("plain", false, "print values without formatting")
 	secretsGetCmd.Flags().Bool("raw", false, "print the raw secret value without processing variables")
 	secretsGetCmd.Flags().Bool("json", false, "output json")
 	secretsCmd.AddCommand(secretsGetCmd)
 
+	secretsSetCmd.Flags().String("project", "", "doppler project (e.g. backend)")
+	secretsSetCmd.Flags().String("config", "", "doppler config (e.g. dev)")
+	secretsSetCmd.Flags().Bool("plain", false, "print values without formatting")
+	secretsSetCmd.Flags().Bool("raw", false, "print the raw secret value without processing variables")
+	secretsSetCmd.Flags().Bool("json", false, "output json")
+	secretsSetCmd.Flags().Bool("silent", false, "don't output the response")
+	secretsCmd.AddCommand(secretsSetCmd)
+
+	secretsDeleteCmd.Flags().String("project", "", "doppler project (e.g. backend)")
+	secretsDeleteCmd.Flags().String("config", "", "doppler config (e.g. dev)")
+	secretsDeleteCmd.Flags().Bool("plain", false, "print values without formatting")
+	secretsDeleteCmd.Flags().Bool("raw", false, "print the raw secret value without processing variables")
+	secretsDeleteCmd.Flags().Bool("json", false, "output json")
+	secretsDeleteCmd.Flags().Bool("silent", false, "don't output the response")
+	secretsCmd.AddCommand(secretsDeleteCmd)
+
+	secretsDownloadCmd.Flags().String("project", "", "doppler project (e.g. backend)")
+	secretsDownloadCmd.Flags().String("config", "", "doppler config (e.g. dev)")
 	secretsDownloadCmd.Flags().Bool("metadata", true, "add metadata to the downloaded file (helps cache busting)")
 	secretsCmd.AddCommand(secretsDownloadCmd)
 
 	rootCmd.AddCommand(secretsCmd)
+}
+
+func printSecrets(secrets map[string]api.ComputedSecret, secretsToPrint []string, jsonFlag bool, plain bool, raw bool) {
+	if len(secretsToPrint) == 0 {
+		for name := range secrets {
+			secretsToPrint = append(secretsToPrint, name)
+		}
+		sort.Strings(secretsToPrint)
+	}
+
+	if jsonFlag {
+		secretsMap := make(map[string]map[string]string)
+		for _, name := range secretsToPrint {
+			if secrets[name] != (api.ComputedSecret{}) {
+				secretsMap[name] = make(map[string]string)
+				secretsMap[name]["computed"] = secrets[name].ComputedValue
+				if raw {
+					secretsMap[name]["raw"] = secrets[name].RawValue
+				}
+			}
+		}
+
+		resp, err := json.Marshal(secretsMap)
+		if err != nil {
+			utils.Err(err)
+		}
+
+		fmt.Println(string(resp))
+		return
+	}
+
+	var matchedSecrets []api.ComputedSecret
+	for _, name := range secretsToPrint {
+		if secrets[name] != (api.ComputedSecret{}) {
+			matchedSecrets = append(matchedSecrets, secrets[name])
+		}
+	}
+
+	if plain {
+		sbEmpty := true
+		var sb strings.Builder
+		for _, secret := range matchedSecrets {
+			if sbEmpty {
+				sbEmpty = false
+			} else {
+				sb.WriteString("\n")
+			}
+
+			if raw {
+				sb.WriteString(secret.RawValue)
+			} else {
+				sb.WriteString(secret.ComputedValue)
+			}
+		}
+
+		fmt.Println(sb.String())
+		return
+	}
+
+	headers := []string{"name", "value"}
+	if raw {
+		headers = append(headers, "raw")
+	}
+
+	var rows [][]string
+	for _, secret := range matchedSecrets {
+		row := []string{secret.Name, secret.ComputedValue}
+		if raw {
+			row = append(row, secret.RawValue)
+		}
+
+		rows = append(rows, row)
+	}
+
+	utils.PrintTable(headers, rows)
 }
