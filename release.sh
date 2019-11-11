11c868d814aae5c7257d3269aea2fe2d11925acd
Thomas Piccirello <thomas@doppler.com>
2019-11-10 22:48:26 -0800

    Always clean up creds file

--- release.sh
+++ release.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function finish {
+  rm "$GOOGLE_APPLICATION_CREDENTIALS"
+}
+trap finish EXIT
+
 echo "$GOOGLE_CREDS" > "$GOOGLE_APPLICATION_CREDENTIALS"
 goreleaser release --rm-dist
-rm "$GOOGLE_APPLICATION_CREDENTIALS"
