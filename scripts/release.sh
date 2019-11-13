68607603ec6b117f1bf4968e53f3357600358040
Thomas Piccirello <thomas@doppler.com>
2019-11-13 03:31:59 -0800

    Use process substitution to avoid writing google creds to filesystem

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,9 +3,6 @@
 set -e
 
 function cleanup {
-  # delete google creds from filesystem
-  rm -f "$GOOGLE_APPLICATION_CREDENTIALS"
-
   # delete docker creds
   set +e
   docker logout $DOCKER_REGISTRY
@@ -18,11 +15,10 @@ trap cleanup EXIT
 echo "Using Docker config $DOCKER_CONFIG"
 
 # save google creds to filesystem
-echo "$GOOGLE_CREDS" > "$GOOGLE_APPLICATION_CREDENTIALS"
 # config will be saved to location explicitly specified in $DOCKER_CONFIG (set by Doppler)
 echo $DOCKER_HUB_TOKEN | docker login -u $DOCKER_HUB_USER --password-stdin $DOCKER_REGISTRY
 echo $GOOGLE_CREDS | docker login -u $GCR_USER --password-stdin $GCR_REGISTRY
 
-goreleaser release --rm-dist
+GOOGLE_APPLICATION_CREDENTIALS="<(echo $GOOGLE_CREDS)" goreleaser release --rm-dist
 scripts/publish-deb.sh
 scripts/publish-rpm.sh
