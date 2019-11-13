e8fdd12395c0e482b7a2a61138bb4675b6959230
Thomas Piccirello <thomas@doppler.com>
2019-11-13 05:12:58 -0800

    Support bumping the version via release type: major/minor/patch

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -8,18 +8,32 @@ if [ ! -z "$(git status --porcelain)" ]; then
 fi
 
 if [ $# -eq 0 ]; then
-  echo "You must specify a version"
+  echo "You must specify a release type or version: major|minor|patch|v1.0.0"
   exit 1
 fi
 
-TAGNAME=$1
-if [ "${TAGNAME:0:1}" == "v" ]; then
-  echo "Version is incorrect; must match format X.Y.Z"
-  exit 1
+RELEASE_TYPE=$1
+PREV_VERSION=$(git describe --abbrev=0)
+
+if [ "${RELEASE_TYPE:0:1}" == "v" ]; then
+  VERSION="$RELEASE_TYPE"
+else
+  reltype=""
+  if [ "$RELEASE_TYPE" == "major" ]; then
+    reltype="M"
+  elif [ "$RELEASE_TYPE" == "minor" ]; then
+    reltype="m"
+  elif [ "$RELEASE_TYPE" == "patch" ]; then
+    reltype="p"
+  else
+    echo "Invalid argument: $RELEASE_TYPE"
+    exit 2
+  fi
+  VERSION=$(./scripts/version.sh "-$reltype" "$PREV_VERSION")
 fi
 
-VERSION="v$TAGNAME"
 echo "Using version $VERSION"
+echo "Previous version: $PREV_VERSION"
 
 # get git in order
 git push
