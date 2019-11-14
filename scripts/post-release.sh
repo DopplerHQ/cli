8f5e284fa165a7e8c606dc662170414a73296e22
Thomas Piccirello <thomas@doppler.com>
2019-11-13 18:49:48 -0800

    Build macOS packages in dist folder

--- scripts/post-release.sh
+++ scripts/post-release.sh
@@ -6,5 +6,8 @@ VERSION=$(git describe --abbrev=0)
 
 echo "Building macOS pkg"
 
-pkgbuild --root dist/doppler_darwin_amd64/ --identifier "com.dopplerhq.cli" --version "${VERSION:1}" --install-location /usr/local/bin doppler.pkg
-productbuild --package doppler.pkg "doppler-$VERSION.pkg"
+pkg_name="dist/doppler.pkg"
+
+pkgbuild --root dist/doppler_darwin_amd64/ --identifier "com.dopplerhq.cli" --version "${VERSION:1}" --install-location /usr/local/bin "$pkg_name"
+productbuild --package "$pkg_name" "dist/doppler-$VERSION.pkg"
+rm "$pkg_name"
