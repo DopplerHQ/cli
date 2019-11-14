97c357ce465821410b70ccd24d95b4ea68019495
Thomas Piccirello <thomas@doppler.com>
2019-11-13 19:39:11 -0800

    Upload macOS pkg as GitHub release artifact

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ build:
 release:
 	./scripts/pre-release.sh $(v)
 	doppler run -- ./scripts/release.sh
-	./scripts/post-release.sh
+	doppler run -- ./scripts/post-release.sh
 
 test-release:
 	goreleaser release --snapshot --skip-publish --skip-sign --rm-dist
--- scripts/post-release.sh
+++ scripts/post-release.sh
@@ -3,11 +3,26 @@
 set -e
 
 VERSION=$(git describe --abbrev=0)
+# remove leading 'v'
+VERSION=${VERSION:1}
 
 echo "Building macOS pkg"
 
-pkg_name="dist/doppler.pkg"
+pkg_name="doppler.pkg"
+final_pkg_name="doppler_$VERSION.pkg"
 
-pkgbuild --root dist/doppler_darwin_amd64/ --identifier "com.dopplerhq.cli" --version "${VERSION:1}" --install-location /usr/local/bin "$pkg_name"
-productbuild --package "$pkg_name" "dist/doppler-$VERSION.pkg"
-rm "$pkg_name"
+# we must use the GitHub release id
+github_release_id=$(curl --silent --show-error https://api.github.com/repos/DopplerHQ/cli/releases/latest  | jq '.id')
+
+# build the package
+pkgbuild --root dist/doppler_darwin_amd64/ --identifier "com.dopplerhq.cli" --version "$VERSION" --install-location /usr/local/bin "dist/$pkg_name"
+productbuild --package "dist/$pkg_name" "dist/$final_pkg_name"
+rm "dist/$pkg_name"
+
+# upload the package
+echo "Upload macOS pkg to GitHub"
+RESPONSE_CODE=$(curl -T "dist/$final_pkg_name" -X POST -u "$GITHUB_USER:$GITHUB_TOKEN" -H "Content-Type: application/octet-stream" $URL -s -w "%{http_code}" -o /dev/null)
+if [[ "$(echo $RESPONSE_CODE | head -c2)" != "20" ]]; then
+  echo "Unable to upload, HTTP response code: $RESPONSE_CODE"
+  exit 1
+fi
