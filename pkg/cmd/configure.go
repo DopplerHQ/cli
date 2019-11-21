0dfb727f121471093a7b1c978895118dbc7656a5
Thomas Piccirello <thomas@doppler.com>
2019-11-21 00:02:20 -0800

    Allow specifying a configure option without an equals sign if only setting one option

--- pkg/cmd/configure.go
+++ pkg/cmd/configure.go
@@ -162,10 +162,21 @@ doppler configure set key=123 otherkey=456`,
 			return errors.New("requires at least 1 arg(s), only received 0")
 		}
 
+		if !strings.Contains(args[0], "=") {
+			if len(args) == 2 {
+				if configuration.IsValidConfigOption(args[0]) {
+					return nil
+				}
+				return errors.New("invalid option " + args[0])
+			}
+
+			return errors.New("too many arguments. To set multiple options, use the format option=value")
+		}
+
 		for _, arg := range args {
 			option := strings.Split(arg, "=")
 			if len(option) < 2 || !configuration.IsValidConfigOption(option[0]) {
-				return errors.New("invalid option " + arg)
+				return errors.New("invalid option " + option[0])
 			}
 		}
 
@@ -173,14 +184,18 @@ doppler configure set key=123 otherkey=456`,
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		silent := utils.GetBoolFlag(cmd, "silent")
-
 		scope := cmd.Flag("scope").Value.String()
-		options := make(map[string]string)
-		for _, option := range args {
-			arr := strings.Split(option, "=")
-			options[arr[0]] = arr[1]
+
+		if !strings.Contains(args[0], "=") {
+			configuration.Set(scope, map[string]string{args[0]: args[1]})
+		} else {
+			options := make(map[string]string)
+			for _, option := range args {
+				arr := strings.Split(option, "=")
+				options[arr[0]] = arr[1]
+			}
+			configuration.Set(scope, options)
 		}
-		configuration.Set(scope, options)
 
 		if !silent {
 			utils.PrintScopedConfig(configuration.Get(scope))
