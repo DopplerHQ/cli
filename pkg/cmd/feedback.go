d01e456f66438ce5045fd6eee8da4c031559fb89
Thomas Piccirello <thomas@doppler.com>
2019-12-10 23:15:02 -0800

    Add feedback command to open GitHub issue

---
+++ pkg/cmd/feedback.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2019 Doppler <support@doppler.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"github.com/DopplerHQ/cli/pkg/utils"
+	"github.com/skratchdot/open-golang/open"
+	"github.com/spf13/cobra"
+)
+
+var feedbackCmd = &cobra.Command{
+	Use:   "feedback",
+	Short: "Provide feedback about the Doppler CLI",
+	Args:  cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := open.Run("https://github.com/DopplerHQ/cli/issues/new/choose")
+		if err != nil {
+			utils.HandleError(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(feedbackCmd)
+}
