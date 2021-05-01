b95413013b11db571eb9bf235ba5232085ce015b
Thomas Piccirello <thomas@doppler.com>
2021-04-30 18:32:06 -0700

    chore: fix version in Slack release notification

--- scripts/release/post-release.sh
+++ scripts/release/post-release.sh
@@ -39,6 +39,7 @@ PACKAGES=$(find dist/*.apk  -type f)
 publishToCloudsmith alpine alpine any-version "$PACKAGES"
 
 # send Slack notification
-MESSAGE="Doppler CLI $CLI_VERSION has been released."
+VERSION=$(git describe --abbrev=0)
+MESSAGE="Doppler CLI v$VERSION has been released."
 curl --tlsv1.2 --proto "=https" -s -X "POST" "$SLACK_WEBHOOK_URL" -H 'Content-Type: application/x-www-form-urlencoded; charset=utf-8' \
   --data-urlencode "payload={\"username\": \"CLI Release Bot\", \"text\": \"$MESSAGE\"}"
